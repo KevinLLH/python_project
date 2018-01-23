3afb22c021d614aee3b582eb9dbaa11e88c8e075
luhui.liu <liuluhui@stu.xjtu.edu.cn>
2018-01-23 14:52:51 +0800

    crawl baidu result

--- baidu_search_demo.py
+++ baidu_search_demo.py
@@ -1,45 +1,82 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'luhui.liu'
+import sys
+import time
 import urllib.request
-from bs4 import BeautifulSoup
-keywords = '小米'
-wd = urllib.request.quote(keywords)
-url = 'http://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=0&rsv_idx=1&ch=12&tn=56060048_4_pg&wd='+wd
-#url = url+'&rsv_pq=ff64d901000031d4&rsv_t=1b7cB%2FKAQLLDfettJz2WrartM10yMWFSwzgBoHAmYV2X6Dv3mqbDMJyJyfplrLpO%2BP8XKA&rsv_enter=1&rsv_sug3=5&rsv_sug1=4&rsv_sug2=0&inputT=6285&rsv_sug4=11467'
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-           'Accept':'text/html;q=0.9,*/*;q=0.8',
-           'Accept-Charset':'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-           'Accept-Encoding':'gzip',
-           'Connection':'close',
-           'Referer':None #注意如果依然不能抓取的话，这里可以设置抓取网站的host
-}
-opener = urllib.request.build_opener()
-opener.addheaders = [headers]
+from bs4  import BeautifulSoup #from BeautifulSoup  import BeautifulSoup 旧的版本，
+import os
+import json
+mymap=['0','1','2','3','4','5','6','7']
 
-page = urllib.request.urlopen(url).read().decode('utf-8')
+#根据关键字获取查询网页
+def baidu_search(key_words,pagenum):
+    url='http://www.baidu.com/s?wd='+key_words+'&pn='+mymap[pagenum]
+    html=urllib.request.urlopen(url).read()
+    return html
 
+ #处理一个要搜索的关键字
+def deal_key(key_words):
+    if os.path.exists('data')==False:
+        os.mkdir('data')
+    filename='data\\'+key_words+'.json'
+    data = []
+    # 打开方式用‘w'时，下边的写要str转换，而对于网页要编码转换，遇到有些不规范的空格还出错
+    fp=open(filename,'w',encoding='utf-8')
+    if fp:
+        pass
+    else:
+        print('文件打失败：'+filename)
+        return
+    x=0
+    while x<=0:
+        htmlpage=baidu_search(key_words,x)
+        soup=BeautifulSoup(htmlpage)
+        for item in soup.findAll("div", {"class": "result"}):                #这个格式应该参考百度网页布局
+            a_click = item.find('a')
+            if a_click:
+                title = a_click.get_text()#标题
+                print("+++++++++++"+title)
+            #fp.write(b'#')
+            if a_click:
+                url = a_click.get("href")
+                #fp.write(url)                 #链接
+            #fp.write(b'#')
+            c_abstract=item.find("div", {"class": "c-abstract"})
+            if c_abstract:
+                strtmp=c_abstract.get_text()
+                #fp.write(strtmp)                                    #描述
+            #fp.write(b'#')
+            data_tmp = {"title":title,"abstract":strtmp,"url":url}
+            print("[JSON]:",data_tmp)
+            #写入json
+            data.append(data_tmp)
+        x=x+1
+        #fp.write(b'\n')
+        print("--------------------")
+    json.dump(data, fp, ensure_ascii=False)
+    fp.close()
 
-html = BeautifulSoup(page)
-for i in range(1,11):
-    res = html.find(id=i)
-    print('第',i,'条结果：')
-    if res.h3 != None:
-        print(res.h3.get_text(strip=True))
-    elif res.h4 != None:
-        print(res.h4.get_text(strip=True))
-    else:
-        print('无法提取标题')
-    if res.find('div',class_='c-abstract') !=None:
-        print(res.find('div',class_='c-abstract').get_text(strip=True))
-    elif res.find('div',class_='c-span18') !=None:
-        if res.find('div',class_='c-span18').p !=None:
-            print(res.find('div',class_='c-span18').p.get_text(strip=True))
-        else:
-            print(res.find('div',class_='c-span18').get_text(strip=True))
-    else:
-        print('无法提取描述')
-    if res.find('span',class_='g') != None:
-        print('网站：',res.find('span',class_='g').get_text(strip=True).split('/')[0])
-    elif res.find('p',class_='op-bk-polysemy-move') !=None:
-        print('网站：',res.find('p',class_='op-bk-polysemy-move').get_text(strip=True).split('/')[0])
-    else:
-        print('无法提取网址')
-    print('\n')
+#读取搜索文件内容，依次取出要搜索的关键字
+def search_file():
+    fp=open('searchfile.txt')
+    i=0
+    keyword=fp.readline()
+    print("[keyword]:"+keyword)
+    while keyword:
+        i=i+1
+        if i==5:
+            print('sleep...')
+            time.sleep(15)
+            print('end...')
+            i=0
+        nPos=keyword.find('\n')
+        if nPos>-1:
+            keyword=keyword[:-1]#keyword.replace('\n','')
+        deal_key(keyword)
+        keyword=fp.readline()
+
+#脚本入口
+print('Start:')
+search_file()
+print('End！')
